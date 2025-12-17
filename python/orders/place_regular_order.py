4419df76f2b305b2c9965bb91e273ebf44e4ea74
akshayn-spec <akshayn@zanskar.xyz>
2025-12-17 16:11:24 +0530

    Create place_regular_order.py

---
+++ python/orders/place_regular_order.py
@@ -0,0 +1,27 @@
+from nubra_python_sdk.start_sdk import InitNubraSdk, NubraEnv
+from nubra_python_sdk.marketdata.market_data import MarketData
+from nubra_python_sdk.trading.trading_data import NubraTrader
+
+nubra = InitNubraSdk(NubraEnv.UAT)
+md = MarketData(nubra)
+
+REF_ID = 1842210
+quote = md.quote(ref_id=REF_ID, levels=5)
+ltp = quote.orderBook.last_traded_price
+
+trade = NubraTrader(nubra, version="V2")
+
+result = trade.create_order({
+    "ref_id": REF_ID,
+    "order_type": "ORDER_TYPE_REGULAR",
+    "order_qty": 1,
+    "order_side": "ORDER_SIDE_BUY",
+    "order_delivery_type": "ORDER_DELIVERY_TYPE_IDAY",
+    "validity_type": "DAY",
+    "price_type": "LIMIT",
+    "order_price": ltp + 50,
+    "exchange": "NSE",
+    "tag": "regular_example"
+})
+
+print(result)
